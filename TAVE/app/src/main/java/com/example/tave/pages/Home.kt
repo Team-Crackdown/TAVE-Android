401e961059abe7a246c1d9faa42ff9f14a88ba1a
luna <eskw0701@naver.com>
2023-06-03 16:41:16 +0900

    unused import 삭제

--- TAVE/app/src/main/java/com/example/tave/pages/Home.kt
+++ TAVE/app/src/main/java/com/example/tave/pages/Home.kt
@@ -3,23 +3,15 @@
 package com.example.tave.pages
 
 import android.content.Context
-import androidx.annotation.DrawableRes
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -27,10 +19,9 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.graphics.toColorInt
 import androidx.navigation.NavController
 import com.example.tave.R
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+
 
 @Composable
 fun homePage(name: String, context: Context, navController: NavController) {
